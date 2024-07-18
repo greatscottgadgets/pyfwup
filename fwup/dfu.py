f964ac10cccabf2c377083f6c438b83bf78f509e
Michael Ossmann <mike@ossmann.com>
2024-07-18 14:19:20 -0400

    dfu: Use timeout instead of delay after DFU_DETACH
    Enable override of default 5 second timeout. Previously there was a
    fixed delay of 3 seconds. Now we allow more time (up to the timeout) but
    may succeed in much less than 3 seconds.

--- fwup/dfu.py
+++ fwup/dfu.py
@@ -125,7 +125,7 @@ class DFUTarget(FwupTarget):
 
 
 
-    def __init__(self, index=0, *args, **kwargs):
+    def __init__(self, index=0, detach=True, timeout=5000, *args, **kwargs):
         """ Creates a new class representing a DFU target.
 
         Accepts the same specifier arguments as pyusb's usb.core.find(); plus an index argument that gets
@@ -154,7 +154,7 @@ class DFUTarget(FwupTarget):
         self.__read_device_info()
 
         # If the device is in runtime mode, send a DFU detach request first.
-        if self.runtime_mode and (self.attributes & self.DFU_WILL_DETACH):
+        if detach and self.runtime_mode and (self.attributes & self.DFU_WILL_DETACH):
             try:
                 self.__dfu_out_request(self.DFU_DETACH, self.interface, None)
             except:
@@ -162,8 +162,18 @@ class DFUTarget(FwupTarget):
             else:
                 # Disconnect device, wait for reenumeration and start over.
                 usb.util.dispose_resources(self.device)
-                time.sleep(3)
-                self.__init__(index=index, *args, **kwargs)
+                start = time.time()
+                while True:
+                    try:
+                        self.__init__(index=index, detach=False, *args, **kwargs)
+                    except BoardNotFoundError:
+                        pass
+                    else:
+                        if not self.runtime_mode:
+                            break
+                    if ((time.time() - start) * 1000) >= timeout:
+                        raise BoardNotFoundError("Device not found after DFU_DETACH.")
+                    time.sleep(0.1)
 
 
     def __read_device_info(self):
